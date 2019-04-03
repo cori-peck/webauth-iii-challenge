a402014208c7b4d2431dfe1fac55a38f472ce9ca
cori-peck <cori.peck@ymail.com>
2019-04-03 17:16:21 -0500

    server/api communicating, register functional

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const helmet = require('helmet');
-const knex = require('knex');
-const knexConfig = require('./knexfile');
+const cors = require('cors');
+const Users = require('./users/user-model');
+const bcrypt = require('bcryptjs');
 
 const server = express();
-const Users = knex(knexConfig.development);
 
 server.use(express.json());
 server.use(helmet());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.status(200).send("Full Authentication Workflow - Challenge III & IV")
+})
+
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 8);
+    user.password = hash;
+
+    Users
+    .add(user)
+    .then(saved => {
+        res.status(201).json(saved);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    })
+})
+
+
 
 const port = process.env.PORT || 8000;
 server.listen(port, () => 
